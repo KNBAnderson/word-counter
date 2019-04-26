d1823891c81dfef1d2dca14195d0ce4541b77f56
Katlin <katlin.NBAnderson@gmail.com>
2019-04-26 11:24:40 -0700

    add logic and test to comapre words regarless of case

--- WordCounter.Tests/ModelTests/Counter.Tests.cs
+++ WordCounter.Tests/ModelTests/Counter.Tests.cs
@@ -18,5 +18,13 @@ namespace WordCounter.Tests
       Assert.AreEqual(1, result);
     }
 
+    [TestMethod]
+    public void WordMatch_CheckIfWordMatchCaps_Int1()
+    {
+      RepeatCounter newCounter = new RepeatCounter();
+      int result = newCounter.WordMatch("cat", "Cat");
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(1, result);
+    }
   }
 }
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -7,6 +7,8 @@ namespace WordCounter.Models
   {
     public int WordMatch(string word, string wordToCompare)
     {
+      word = word.ToLower();
+      wordToCompare = word.ToLower();
       if (word == wordToCompare)
       {
         return 1;
