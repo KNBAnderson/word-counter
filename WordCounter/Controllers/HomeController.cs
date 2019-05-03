8bf0718922ce35667c34366f7576ee66fcb489a3
Katlin <katlin.NBAnderson@gmail.com>
2019-05-03 09:53:39 -0700

    complete basic file structure for Tests

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -7,17 +7,17 @@ namespace WordCounter.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/input")]
-    public ActionResult Input() { return View(); }
-
-    [Route("/output")]
-    public ActionResult Output(string input1, string input2)
-    {
-      ProjectVariable myProjectVariable = new ProjectVariable();
-      myProjectVariable.SetInput1(input1);
-      myProjectVariable.SetInput2(input2);
-      return View(myProjectVariable);
-    }
+    // [Route("/input")]
+    // public ActionResult Input() { return View(); }
+    //
+    // [Route("/output")]
+    // public ActionResult Output(string input1, string input2)
+    // {
+    //   ProjectVariable myProjectVariable = new ProjectVariable();
+    //   myProjectVariable.SetInput1(input1);
+    //   myProjectVariable.SetInput2(input2);
+    //   return View(myProjectVariable);
+    // }
 
   }
 }
